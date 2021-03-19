39c5da181f9cca7e692d9699a67ff1bb1129ac27
Gaetano Fresa <fischkotpf@hotmail.de>
2021-03-19 22:16:18 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@ set -x
 apt update && apt upgrade && apt install cups printer-driver-splix
 sudo usermod -aG lpadmin $USER 
 systemctl restart cups.service
+sudo cupsctl --remote-admin
+sudo cupsctl --share-printers
 set +x
